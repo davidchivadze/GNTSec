900e7ffb4e57573774c102a12bae81fd24c9ca47
Nika Panozishvili <npanozishvili@NPANOZISHVILI>
2021-06-13 23:06:48 +0400

    commit

--- src/app/http/administration/import/service/file-import.service.ts
+++ src/app/http/administration/import/service/file-import.service.ts
@@ -1,5 +1,5 @@
 ﻿import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import {FileImportRequestModel} from "../../../../model/import/file-import.model";
 import {Observable} from "rxjs";
 import {map} from "rxjs/operators";
@@ -15,12 +15,6 @@ export class FileImportService {
     let formData = new FormData();
     formData.append('file', request.file);
     formData.append('importType', request.importType.toString());
-
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'multipart/form-data'
-      })
-    };
 
     return this.http.post<boolean>(`http://localhost:5000/ImportJob/FileImport`, formData)
       .pipe(map(response => {
